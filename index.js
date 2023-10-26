3a3bea9016b6d55e9ba36018a9d0ff44c457ddf4
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-10-26 15:19:33 +0200

    Controllo sui valori inseriti

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let chilometri = parseFloat(prompt("Quanti chilometri devi percorrere?"));
-let eta = parseInt(prompt("Quanti anni hai?"));
-let prezzo = 0.21 * chilometri;
-let sconto;
-if(eta < 18){
-    sconto = prezzo * 20 / 100;
-    prezzo = prezzo - sconto;
-}else if(eta > 65){
-    sconto = prezzo * 40 / 100;
-    prezzo = prezzo - sconto;
+const chilometri = parseFloat(prompt("Quanti chilometri devi percorrere?"));
+const eta = parseInt(prompt("Quanti anni hai?"));
+if(!(isNaN(chilometri) == true || isNaN(eta) == true)){
+    let prezzo = 0.21 * chilometri;
+    let sconto;
+    if(eta < 18){
+        sconto = prezzo * 20 / 100;
+        prezzo = prezzo - sconto;
+    }else if(eta > 65){
+        sconto = prezzo * 40 / 100;
+        prezzo = prezzo - sconto;
+    }
+    prezzo = prezzo.toFixed(2);
+    const prezzoContainer = document.getElementById("prezzoContainer");
+    prezzoContainer.innerHTML = prezzo;
+}else{
+    const messaggioContainer = document.getElementById("messaggioContainer");
+    messaggioContainer.innerHTML = "Errore durante l'immissione dei dati. Assicurati di inserire dei numeri."
 }
-prezzo = prezzo.toFixed(2);
-let prezzoContainer = document.getElementById("prezzoContainer");
-prezzoContainer.innerHTML = prezzo;
